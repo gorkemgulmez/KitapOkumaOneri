606ccb45d354f2d848a12b0b60f13ecd57881ee6
gorkemgulmez <gorkemgulmez7@gmail.com>
2018-10-20 21:41:44 +0300

    Database class init

---
+++ src/database/Database.java
@@ -0,0 +1,35 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.collections.ObservableList;
+
+public class Database {
+
+	private static Connection connection;
+
+	public static void setConnection() {
+		String url, username, password;
+		url = "jdbc:mysql://localhost:3306/Kitap?useUnicode=yes&characterEncoding=UTF-8";
+		username = "admin";
+		password = "333ordEMmgH8aJud";
+
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			System.out.println("Error while accessing database");
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("Success");
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+}
