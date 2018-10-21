010c0158d0a7c154684a89ea93061aae4cb630ff
gorkemgulmez <gorkemgulmez7@gmail.com>
2018-10-21 16:25:10 +0300

    fix

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,51 +3,67 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 
 public class Main extends Application {
 	
 	private static Stage window;
 	
-	@FXML
-	private Button login;
-	// private Button validate;
-	
-	@FXML
-	protected static void sceneRedirect_mainWindow() throws IOException {
-		Parent root = FXMLLoader.load(Main.class.getResource("MainWindow.fxml"));
-		Scene scene = new Scene(root, 500, 600);
-		window.setScene(scene);
-		window.setTitle("Kitap");
+	protected static void sceneRedirect_mainWindow() {
+		Parent root;
+		try {
+			root = FXMLLoader.load(Main.class.getResource("MainWindow.fxml"));
+			Scene scene = new Scene(root, 500, 600);
+			window.setScene(scene);
+			window.setTitle("Kitap");
+		} catch (IOException e) {
+			System.out.println("Could not found MainWindow.fxml");
+			e.printStackTrace();
+		}
 	}
 	
-	@FXML
-	protected static void sceneRedirect_register() throws IOException {
-		Parent root = FXMLLoader.load(Main.class.getResource("Register.fxml"));
-		Scene scene = new Scene(root, 500, 600);
-		window.setScene(scene);
-		window.setTitle("Kayýt Ol");
+	protected static void sceneRedirect_register() {
+		Parent root;
+		try {
+			root = FXMLLoader.load(Main.class.getResource("Register.fxml"));
+			Scene scene = new Scene(root, 500, 600);
+			window.setScene(scene);
+			window.setTitle("Kayýt Ol");
+		} catch (IOException e) {
+			System.out.println("Could not found Register.fxml");
+			e.printStackTrace();
+		}
 	}
 	
-	@FXML
-	protected static void sceneRedirect_logIn() throws IOException {
-		Parent root = FXMLLoader.load(Main.class.getResource("LogIn.fxml"));
-		Scene scene = new Scene(root, 500, 600);
-		window.setScene(scene);
-		window.setTitle("Giriþ");
+	protected static void sceneRedirect_logIn() {
+		Parent root;
+		try {
+			root = FXMLLoader.load(Main.class.getResource("LogIn.fxml"));
+			Scene scene = new Scene(root, 500, 600);
+			window.setScene(scene);
+			window.setTitle("Giriþ");
+		} catch (IOException e) {
+			System.out.println("Could not found LogIn.fxml");
+			e.printStackTrace();
+		}
+		
 	}
 
-	@FXML
-	protected static void sceneRedirect_firstLogIn() throws IOException {
-		Parent root = FXMLLoader.load(Main.class.getResource("FirstLogIn.fxml"));
-		Scene scene = new Scene(root, 500, 600);
-		window.setScene(scene);
-		window.setTitle("Oylama");
+	public static void sceneRedirect_firstLogIn() {
+		Parent root;
+		try {
+			root = FXMLLoader.load(Main.class.getResource("FirstLogIn.fxml"));
+			Scene scene = new Scene(root, 500, 600);
+			window.setScene(scene);
+			window.setTitle("Oylama");
+		} catch (IOException e) {
+			System.out.println("Could not found FirstLogIn.fxml");
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@Override
--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -8,11 +8,10 @@ import javafx.scene.text.Text;
 
 public class RegisterController {
 
-	@FXML
-	private static TextField username;
-	private static PasswordField password;
-	private static Text ID = new Text("ID: ");
-	private static Text adress = new Text("Adres: ");
+	@FXML private static TextField username;
+	@FXML private static PasswordField password;
+	@FXML private static Text ID = new Text("ID: ");
+	@FXML private static Text adress = new Text("Adres: ");
 	
 	@FXML
 	protected static void registerConfirm() throws IOException {
